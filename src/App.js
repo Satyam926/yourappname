6736bd25658b2de4e8ce33957fa54d0d9ba951fa
Satyam926 <satyamt926@gmail.com>
2022-01-23 00:54:40 +0530

    React Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import './App.css';
+import { DataProvider } from './CategoryContext';
+import Bollywood from './Components/Category/Bollywood';
+import Fitness from './Components/Category/Fitness';
+import Food from './Components/Category/Food';
+import Hollywood from './Components/Category/Hollywood';
+import Technology from './Components/Category/Technology';
+import './Components/Style/Style.css'
+import Header from './Header';
+import Home from './Home';
+import ArtReading from './Components/ArtReading';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      
+      <BrowserRouter>
+         <Header/>
+         <Routes>
+           <Route path='/' element={<Home/>}/>
+           <Route path='/home' element={<Home/>}/>
+           <Route path='/bollywood' element={<Bollywood/>}/>
+           <Route path='/hollywood' element={<Hollywood/>}/>
+           <Route path='/fitness' element={<Fitness/>}/>
+           <Route path='/technology' element={<Technology/>}/>
+           <Route path='/food' element={<Food/>}/>
+           <Route path='/artReading/:stdID' element={<ArtReading/>}/>
+
+         </Routes>
+      </BrowserRouter>
+       
+
+    </DataProvider>
   );
 }
-
 export default App;
+
+ 
